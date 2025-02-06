9951f27a348478e72d63df2970831082843b17a0
TaohidHossain <htaohid31@gmail.com>
2025-02-06 11:27:52 +0600

    chore: start over client side

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl text-red-200">Test</h1>
+      <p className="text-lg font-semibold">Test</p>
+    </>
+  )
+}
+
+export default App
