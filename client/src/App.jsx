c9c9570703c55515dfd69a5eeae0e942d1752fac
TaohidHossain <htaohid31@gmail.com>
2025-02-09 17:42:56 +0600

    feat: add email verification

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Dashboard from './pages/Dashboard'
+import VerifyEmail from './pages/VerifyEmail'
 function App() {
 
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Dashboard />}/>
       <Route path="/login" element={<Login />}/>
       <Route path="/signup" element={<Signup />}/>
+      <Route path="/email/verify/:code" element={<VerifyEmail />}/>
     </Routes>
   )
 }
--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -2,6 +2,7 @@ import API from '../config/apiClient'
 
 export const login = async data => API.post("auth/login", data);
 export const signup = async data => API.post("auth/signup", data);
+export const verifyEmail = async code => API.get(`auth/email/verify/${code}`);
 
 
 export const getUnassignedRole = async () => API.get("/role/unassigned");
