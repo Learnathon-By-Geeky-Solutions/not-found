268f3099d2a570c921b56d33192f31bcfe95a809
TaohidHossain <htaohid31@gmail.com>
2025-02-09 12:11:14 +0600

    feat: add getUnassigedRole controller

--- server/src/routes/v1/v1.route.ts
+++ server/src/routes/v1/v1.route.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRouter from "./auth.route";
 import userRouter from "./user.route";
+import roleRouter from "./role.route";
 
 const v1Router = Router();
 
 v1Router.use("/auth", authRouter);
 v1Router.use("/user", userRouter);
+v1Router.use("/role", roleRouter);
 
 export default v1Router;
